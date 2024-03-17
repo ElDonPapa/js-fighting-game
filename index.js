5940807e7a17a97cd5cb31a4352fdadd93decabf
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-17 22:31:47 +0100

    Add main game update loop

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const player1 = new Sprite(c, {x: 10, y: 10});
 player1.draw();
 
+// Main game loop
+let lastTime;
+function update(time)
+{
+    if(lastTime !== null)
+    {
+        const delta = time - lastTime;
+        // Put game updates here
+    }
+
+    lastTime = time;
+    window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
