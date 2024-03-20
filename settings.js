1f600c2d647caf0af4b6193b83dd4bc07e2b4ea6
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-20 17:12:20 +0100

    Add gravity and floor collision detection to the sprite

---
+++ settings.js
@@ -0,0 +1,5 @@
+export default class Settings {
+    static canvasWidth = 1024;
+    static canvasHeight = 576;
+    static gravity = 0.5;
+}
--- sprite.js
+++ sprite.js
@@ -1,20 +1,36 @@
+import Settings from "./settings.js"
+
 export default class Sprite {
     constructor(c){
         this.c = c;
+        this.height = 130;
         this.position = {
             x: 0,
             y: 0
         };
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
     }
 
     draw() {
         this.c.fillStyle = "red";
-        this.c.fillRect(this.position.x, this.position.y, 50, 130);
+        this.c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    updateGravity() {
+        if((this.height + this.position.y + this.velocity.y) < Settings.canvasHeight){
+            this.velocity.y += Settings.gravity;
+        } else {
+            this.velocity.y = 0;
+        }
     }
 
     update() {
         this.draw();
-        this.position.y++;
+        this.position.y += this.velocity.y;
+        this.updateGravity();
     }
 }
 
