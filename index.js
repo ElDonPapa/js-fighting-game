8527795dbe827cfd7c758887b256799aaf4a33ba
ElDonPapa <26351793+ElDonPapa@users.noreply.github.com>
2024-03-23 22:54:22 +0100

    Add attack for player 2

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function update(time)
             player2.velocity.x = -player2.moveSpeed;
         }
         
-        // Attack box collisions check
+        // Player 1 Attack box collisions check
         if(
             player1.attackBox.position.x + player1.attackBox.width > player2.position.x
             && player1.attackBox.position.x < player2.position.x + player2.width
@@ -81,7 +81,19 @@ function update(time)
             && player1.isAttacking
         ) {
             player1.isAttacking = false;
-            console.log("ATTACK");
+            console.log("ATTACK FROM P1");
+        }
+
+        // Player 1 Attack box collisions check
+        if(
+            player2.attackBox.position.x + player2.attackBox.width > player1.position.x
+            && player2.attackBox.position.x < player1.position.x + player1.width
+            && player2.attackBox.position.y + player2.attackBox.height > player1.position.y
+            && player2.attackBox.position.y < player1.position.y + player1.height
+            && player2.isAttacking
+        ) {
+            player2.isAttacking = false;
+            console.log("ATTACK FROM P2");
         }
     }
 
@@ -110,7 +122,7 @@ window.addEventListener("keydown", (event) => {
             }
             break;
         case "s":
-            player1.attack()
+            player1.attack();
             break; 
 
         // Player 2
@@ -127,6 +139,9 @@ window.addEventListener("keydown", (event) => {
                 player2.canJump = false;
                 player2.velocity.y = -player2.jumpHeight;
             }
+            break;
+        case "ArrowDown":
+            player2.attack();
             break;
     }
 })
